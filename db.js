940f71d8f0eb818ae40d6113864a5656f44c5e6b
Henrycoding-design <tanbinhvo.hcm@gmail.com>
2025-09-14 12:23:16 +0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const conn = process.env.DATABASE_URL || '';
+const needsSSL = /render\.com/i.test(conn); // true for your Render URL, false for localhost
+
+const pool = new Pool({
+  connectionString: conn,
+  ssl: needsSSL ? { rejectUnauthorized: false } : false
+});
+
+module.exports = { pool };
+
+// SSL  = Secure Sockets Layer
